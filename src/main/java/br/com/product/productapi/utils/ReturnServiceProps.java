2313629a788e3f0672e88d3c5cb5dc8d22be978e
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-08-19 20:49:07 -0300

    ReturnServiceProps

---
+++ src/main/java/br/com/product/productapi/utils/ReturnServiceProps.java
@@ -0,0 +1,55 @@
+package br.com.product.productapi.utils;
+
+import br.com.product.productapi.models.Stock;
+import br.com.product.productapi.shared.ProductDto;
+
+public class ReturnServiceProps {
+    private boolean isStockServiceOk;
+    private Stock stock;
+    private Exception exception;
+    public Exception getException() {
+        return exception;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+    private boolean hasFreeSpace;
+    private ProductDto productDto;
+    
+    public ReturnServiceProps() {}
+    
+    public ReturnServiceProps(
+    Stock stock,
+    boolean hasFreeSpace, 
+    ProductDto productDto) {
+        this.stock = stock;
+        this.hasFreeSpace = hasFreeSpace;
+        this.productDto = productDto;
+    }
+
+    public boolean isStockServiceOk() {
+        return isStockServiceOk;
+    }
+    public void setStockServiceOk(boolean isStockServiceOk) {
+        this.isStockServiceOk = isStockServiceOk;
+    }
+    public Stock getStockExists() {
+        return stock;
+    }
+    public void setStockExists(Stock stock) {
+        this.stock = stock;
+    }
+    public boolean isHasFreeSpace() {
+        return hasFreeSpace;
+    }
+    public void setHasFreeSpace(boolean hasFreeSpace) {
+        this.hasFreeSpace = hasFreeSpace;
+    }
+    public ProductDto getProductDto() {
+        return productDto;
+    }
+    public void setProductDto(ProductDto productDto) {
+        this.productDto = productDto;
+    }
+}
