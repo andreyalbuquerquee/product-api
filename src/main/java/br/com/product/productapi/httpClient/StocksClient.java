3aa575e47afd5ef0aeb9b01f5109476375408a9c
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-08-19 20:48:24 -0300

    StocksClients

--- src/main/java/br/com/product/productapi/httpClient/StocksClient.java
+++ src/main/java/br/com/product/productapi/httpClient/StocksClient.java
@@ -1,5 +1,6 @@
 package br.com.product.productapi.httpClient;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
